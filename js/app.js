ef990838282304534cead9a7f45e69acd1f2af50
Piotr Kulpa <p.kulpa@onet.eu>
2018-02-25 13:49:40 +0100

    feat: add list of cards, add function to display cards

--- js/app.js
+++ js/app.js
@@ -1,16 +1,19 @@
 /*
  * Create a list that holds all of your cards
  */
-
-
+ const cardList = document.querySelectorAll('.deck li');
+ 
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
-
-
+ // copy all li el
+ //shufle li el
+ //delete old li el
+ //append shuffled el
+ function displayCards () {}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
