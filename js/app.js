af5c2ad60dd0db8d981d2ffebcbeec5dd763cbd3
Piotr Kulpa <p.kulpa@onet.eu>
2018-03-04 11:53:19 +0100

    feat: stars, time of game prevent double click

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-/*
- * Create a list that holds all of your cards
+ /**
+ * @description DOM elements and temporary files.
  */
 let cardList = document.querySelectorAll('.deck li');
 let arrOfCards = [];
@@ -10,14 +10,43 @@ let openCardsArr = [];
 let counter = 0;
 let displayMoves = document.querySelector('.moves');
 let restart = document.querySelector('.restart');
+let timeOfGame = 0;
+let openedCard;
+let stars = document.querySelectorAll('.stars li');
+let startTimer;
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
+ /**
+ * @function
+ * @description Initialize function.
  */
+ function init() {
+   clearTimeout(startTimer);
+   displayMoves.innerText = 0;
+   timeOfGame = 0;
+   openCardsArr = [];
+   counter = 0;
+   displayCards();
+   stopwatch();
+   initClickCard();
+   stars.forEach(function(el) {
+     el.style.display = 'inline-block';
+   });
+ }
 
+ /**
+ * @function
+ * @description Initialize card click.
+ */
+ function initClickCard() {
+   for (let x = 0; x < cards.length; x++) {
+    cards[x].addEventListener('click', clickCard);
+   }
+ }
+
+ /**
+ * @function
+ * @description Create a list that holds all of your cards.
+ */
  function displayCards() {
    //convert nodeList to array
    for (let i = 0; i < cardList.length; i++) {
@@ -36,7 +65,10 @@ let restart = document.querySelector('.restart');
    deck.appendChild(documentFragment);
  }
 
-// Shuffle function from http://stackoverflow.com/a/2450976
+/**
+* @function
+* @description Shuffle function from http://stackoverflow.com/a/2450976.
+*/
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -50,62 +82,130 @@ function shuffle(array) {
     return array;
 }
 
-displayCards();
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ /**
+ * @function
+ * @description Open card.
  */
-
  function displaySymbol(e) {
    e.target.classList.add('show', 'open');
  }
 
+ /**
+ * @function
+ * @description Opened cards list.
+ */
  function openedCardsList(e) {
+   /** Insert first(odd) opened card to list of opened cards*/
   openCardsArr.push(e.target.firstElementChild.className);
+  /** Set second(even) opened card*/
+  openedCard = e.target;
  }
 
+ /**
+ * @function
+ * @description Check opened cards list.
+ */
  function checkList(e) {
-   if (openCardsArr.includes(e.target.firstElementChild.className)) {
-     var similarCards =  document.getElementsByClassName(e.target.firstElementChild.className);
-     for (let a = 0; a < similarCards.length; a++) {
-       similarCards[a].parentNode.classList.add('match');
-     }
-   };
+   /** Check even clicks. */
+   if (counter % 2 === 0) {
+     /** Check if cards match. */
+     /** If cards match. */
+     if (openCardsArr.includes(e.target.firstElementChild.className)) {
+       /** Get similar cards.*/
+       var similarCards =  document.getElementsByClassName(e.target.firstElementChild.className);
+       /** Set style to 'match'. */
+       for (let a = 0; a < similarCards.length; a++) {
+         similarCards[a].parentNode.classList.add('match');
+       };
+       /** Remove click from matched cards. */
+       e.target.removeEventListener('click', clickCard);
+       openedCard.removeEventListener('click', clickCard);
+       /** Show final scores if all cards match. */
+       finalScore();
+      /** If cards not match.*/
+     } else {
+       /** Remove click from not matched cards. */
+       e.target.removeEventListener('click', clickCard);
+       openedCard.removeEventListener('click', clickCard);
+       /** Show not matched cards with red background.*/
+       openedCard.classList.add('not-match');
+       e.target.classList.add('not-match');
+       /** After 1 s. hide cards.*/
+       let delay = setTimeout(function () {
+         /** Add again click to not matched cards. */
+         e.target.addEventListener('click', clickCard);
+         openedCard.addEventListener('click', clickCard);
+         /** Hide not matched cards.*/
+         e.target.className = 'card';
+         openedCard.classList.remove('show', 'open', 'not-match');
+         /** Remove last card from list of opened cards. */
+         openCardsArr.splice(openCardsArr.length -1, 1);
+         clearTimeout(delay);
+       }, 500);
+
+     };
+     /** Open card when odd click. */
+   } else {
+     /** Add this card to list of opened cards. */
+     openedCardsList(e);
+     /** Prevent second click on same card. */
+     e.target.removeEventListener('click', clickCard);
+   }
  }
 
+ /**
+ * @function
+ * @description Display moves and stars.
+ */
  function incrementCounter() {
    counter++;
    displayMoves.innerText = counter;
- }
-
- function finalScore() {
-   if(openCardsArr.length >= 8) {
-     alert(`Finished with ${counter} moves`);
-     // for(let i = 0; i < cards.length; i++) {
-     //   cards[i].style.display = 'none';
-     // };
+   switch(true) {
+     case counter > 20 && counter < 24:
+      stars[2].style.display = 'none';
+      break;
+     case counter > 24:
+      stars[1].style.display = 'none';
+      break;
    };
  }
 
-for (let x = 0; x < cards.length; x++) {
- cards[x].addEventListener('click', function(e) {
+ /**
+ * @function
+ * @description Display final crores.
+ */
+ function finalScore() {
+   if(openCardsArr.length === 8) {
+     alert(`Finished with ${counter} moves in ${timeOfGame} seconds`);
+     clearTimeout(startTimer);
+   };
+ }
+
+ /**
+ * @function
+ * @description After click card increment counter, display this card symbol and check opened cards list.
+ */
+ function clickCard(e) {
+   incrementCounter();
    displaySymbol(e);
    checkList(e);
-   openedCardsList(e);
-   incrementCounter();
-   finalScore();
- });
+ }
+
+ /**
+ * @function
+ * @description Set timer.
+ */
+ function stopwatch() {
+   startTimer = setTimeout(function () {
+     timeOfGame++;
+     stopwatch();
+ }, 1000);
 }
 
+/** Initialize game*/
+init();
+
+/** Restart game*/
 restart.addEventListener('click', function(e) {
-  displayCards();
-  openCardsArr = [];
-  counter = 0;
+  init();
 });
